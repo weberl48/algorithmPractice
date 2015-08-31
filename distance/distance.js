d39fcf2eff8bcf1c6e135dd30ac22759fb221b23
Lucas Weber <weberl48@outlook.com>
2015-08-31 09:35:01 -0600

    added player and flag instances

--- distance/distance.js
+++ distance/distance.js
@@ -1,4 +1,5 @@
 
+ //solution 1
 player = {
   x: 5,
   y:4
@@ -13,15 +14,19 @@ function Distance (player, flag) {
   var ans = Math.sqrt(x);
   return ans;
 }
-
+//solution 2
 function Coordinates (x,y,type) {
   this.x = x;
   this.y = y;
   this.type = type;
 
 }
+var player = new Coordinates(2,0,'Person');
+var flag = new Coordinates(3,1, 'Flag');
+
+findDistance(player,flag);
 
 function findDistance (node1, node2) {
-  return Math.sqrt(Math.pow((node2.x - node1.x), 2) + Math.pow((node2.y - node1.y), 2));
+  // return Math.sqrt(Math.pow((node2.x - node1.x), 2) + Math.pow((node2.y - node1.y), 2));
   return Math.hypot(node2.x-node1.x, node2.y - node1.y);
 }
