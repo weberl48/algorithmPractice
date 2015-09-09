342518bd80db2ffdcb748e5f97640f131dccfca3
Lucas Weber <weberl48@outlook.com>
2015-09-09 00:26:36 -0600

    binary conversion

---
+++ binaryConversion/binaryConversion.js
@@ -0,0 +1,6 @@
+function BinaryConverter(str) {
+
+  return parseInt(str ,2);
+
+}
+// The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
