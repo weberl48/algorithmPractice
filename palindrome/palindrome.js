244e5d212729390d2ad4c090f9f0e603d4c02bcd
Lucas Weber <weberl48@outlook.com>
2015-09-20 08:16:22 -0600

    palindrome

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -19,4 +19,14 @@ function isPalindrome(str){
 function checkPalindrom(str) {
     return str == str.split('').reverse().join('');
 }
-  
+
+// Have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string.
+function Palindrome(str) {
+	if (str.split("").reverse().join("") === str) {
+
+    	return true
+    } else {
+  // code goes here
+  return false;
+    }
+}
