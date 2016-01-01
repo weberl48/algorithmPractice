ff50fc3f453fe3ee12639b08f38dc4e7df118207
Lucas Weber <weberl48@outlook.com>
2015-12-31 23:13:22 -0500

    odd fib sum

---
+++ Fibonacci/oddFib.js
@@ -0,0 +1,15 @@
+function sumFibs(num) {
+    var prevNumber = 0;
+    var currNumber = 1;
+    var result = 0;
+    while (currNumber <= num) {
+        if (currNumber % 2 !== 0) {
+            result += currNumber;
+        }
+        var added = currNumber + prevNumber;
+        prevNumber = currNumber;
+        currNumber = added;
+    }
+
+    return result;
+}
